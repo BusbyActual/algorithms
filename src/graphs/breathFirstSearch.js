224d51e8ba9b0dffe57d7fba809f6e5560080c42
Ryan Busby <BusbyActual@gmail.com>
2018-06-04 22:10:05 -0700

    Solved prompt

--- src/graphs/breathFirstSearch.js
+++ src/graphs/breathFirstSearch.js
@@ -16,32 +16,25 @@ let graph =  {
   ]}]
 };
 
-// console.log(graph.children);
 
 let BFS = (graph, target) => {
   let queue = [graph];
 
   while(queue.length) {
-    // console.log(x, JSON.stringify(queue), '##')
     let current = queue.shift();
-    console.log(current.value, target, queue.length)
 
     if (current.value === target) {
-      console.log('winnnn', current)
       return current;
     } else {
       if (current.children.length) {
-        console.log(current.children)
         current.children.forEach((child) => {
           queue.push(child);
         });
-        x = 0;
-        console.log(JSON.stringify(queue))
       }
     }
-
-    console.log(queue, x)
   }
 };
 
-BFS(graph, 'E');
+let work = BFS(graph, 'E');
+
+console.log(work);
