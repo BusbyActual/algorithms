a9e7030c6ff8acc33630bd8b541675ed737579ad
Ryan Busby <BusbyActual@gmail.com>
2018-06-04 20:46:56 -0700

    solved

--- src/sort-search/bubbleSort.js
+++ src/sort-search/bubbleSort.js
@@ -1,12 +1,35 @@
-let swap = (left, right) => {
-  let temp = left;
-  left = right;
-  right = temp;
-};
-
 let input =  [5,1,4,2,8];
 
-
 let bubbleSort = (arr) => {
+  let swapped = false;
+  for (let x = 0; x < arr.length - 1; x++) {
+    let left = arr[x];
+    let right = arr[x + 1];
 
+    if (right < left) {
+      let temp = arr[x + 1];
+      arr[x + 1] = arr[x];
+      arr[x] = temp;
+
+      swapped = true;
+    }
+  }
+
+  if (!swapped ) {
+    console.log(arr);
+    return arr;
+  } else {
+    console.log(arr);
+    bubbleSort(arr);
+  }
 };
+
+let init = (arr) => {
+  if (Array.isArray(arr)) {
+    bubbleSort(arr);
+  } else {
+    return '' + typeof(arr) + ' is not of type Array';
+  }
+};
+
+init(input);
