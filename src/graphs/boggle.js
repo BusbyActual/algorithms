167ba88617e36c0f56203d66551768f2a62e31cc
Ryan Busby <BusbyActual@gmail.com>
2018-06-09 11:34:42 -0700

    init prompt

---
+++ src/graphs/boggle.js
@@ -0,0 +1,14 @@
+let words = ["GEEKS", "FOR", "QUIZ", "GO"];
+let graph = [ ['G','I','Z'],
+              ['U','E','K'],
+              ['Q','S','E'] ];
+
+let boggle = (graph, targets) => {
+  let Answer = [];
+
+  return Answer;
+};
+
+let work = boggle(graph, words);
+
+console.log(work);
